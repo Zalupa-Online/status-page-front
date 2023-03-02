ba315ec27f0d997a1b3ad54c24fa4d84cd8983e8
Koval Yaroslav <ukraine24@icloud.com>
2023-03-03 00:10:34 +0200

    update

--- logic.js
+++ logic.js
@@ -59,3 +59,43 @@ const getStatus = (callback) => {
         callback(r);
     }, "https://api-status.zalupa.online/", "GET", true, null);
 }
+
+const rebuildBlocks = () => {
+    getStatus(function(resp) {
+        const selector = document.getElementById("status_container");
+
+        const mc = resp.minecraft;
+        const http = resp.http;
+
+        const template = (service) => {
+            const status = service.status ? "up" : "down";
+            return `
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" style="
+                            background: url(service_${status}_bg.png);
+                            background-position: center;
+                            background-repeat: no-repeat;
+                            background-size: cover;
+                        "><title>${service.title}</title><text x="50%" y="50%" fill="#2a2a2a" dy=".3em">${service.title}</text></svg>
+                        <div class="card-body">
+                            <p class="card-text">${service.latency}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        };
+
+        selector.innerHTML = "";
+        for (let i = 0; i < mc; i++) {
+            selector.innerHTML = selector.innerHTML + template(mc[i]);
+        }
+        for (let i = 0; i < http; i++) {
+            selector.innerHTML = selector.innerHTML + template(http[i]);
+        }
+    });
+}
+
+window.onload = () => {
+    setInterval(rebuildBlocks, 1000);
+}
